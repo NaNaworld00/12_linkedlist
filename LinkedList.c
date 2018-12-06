374a9ea40738489e8af893d7eb72740b3ad59fb3
NaNaworld00 <subiki@naver.com>
2018-12-06 15:43:15 +0900

    linknd_3

--- LinkedList.c
+++ LinkedList.c
@@ -1,8 +1,30 @@
-typedef struct linknode {
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linknd {
 	int data;
 	void *next;
-	//struct linknode *next;
+	//struct linknd *next;
 } linknd_t;
 
 
 static linknd_t *list; //linked List 실체 
+
+linknd_t* create_rode(int value) {
+	linknd_t* ndPtr;
+	
+	//동적 메모리 할당
+	ndPtr = (linknd_t*)malloc(sizeof(linknd_t));
+	if (ndPtr == NULL)
+	{
+		printf("ERROR\n");
+		return NULL;
+	 } 
+	
+	//정수값 저 장
+	ndPtr->data = value;
+	ndPtr->next = NULL;
+	
+	return ndPtr;
+	 
+}
