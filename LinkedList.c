4b3a5b3d1c70d9e6a30608205a1ab680ac4b30d3
NaNaworld00 <subiki@naver.com>
2018-12-06 16:33:59 +0900

    linknd_realfinal

--- LinkedList.c
+++ LinkedList.c
@@ -53,12 +53,19 @@ void genList(void){
 	list = create_node(0);
 	return;
 }
-void print_list(void){
+
+void print_node(int n){
 	linknd_t *ndPtr;
+	int i=0;
 	
 	ndPtr = (linknd_t*)list->next;
 	while(ndPtr != NULL){
-		printf("%i ", ndPtr->data);
+		i++;
+		if (i == n)
+		{
+			printf("%i-th value : %i\n", i, ndPtr->data);
+			break;
+		}
 		ndPtr = ndPtr->next;
 	}
 }
--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,3 @@
-void print_list(void);
+void print_node(int n);
 void genList(void);
 void addTail(int value);
